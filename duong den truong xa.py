300726ed0b34cc48935821987b301678ccfbd7e7
HuynhThiMyDuyen01 <hmyduyen47@gmail.com>
2024-08-18 17:01:24 +0700

    Add files via upload

---
+++ duong den truong xa.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 18 16:58:54 2024
+
+@author: MY DUYEN
+"""
+
+distance=float(input("nhap do dai doan duong (m):"))
+if distance > 1200:
+    print("duong den truong qua xa")
