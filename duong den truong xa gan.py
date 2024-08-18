4c1d47603ac90a0f01367d1eb913362d869eb407
HuynhThiMyDuyen01 <hmyduyen47@gmail.com>
2024-08-18 17:00:59 +0700

    Add files via upload

---
+++ duong den truong xa gan.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 18 17:00:38 2024
+
+@author: MY DUYEN
+"""
+
+distance=float(input("nhap do dai doan duong den truong  (m):"))
+if distance > 1200:
+    print("duong den truong qua xa")
+else:
+    print("khong xac dinh duoc xa-gan ")
